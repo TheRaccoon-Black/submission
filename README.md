2917533b2b8e7fcbbc8a492a7588199989275257
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-03-04 16:41:13 +0700

    first submission

---
+++ README.md
@@ -0,0 +1,35 @@
+# Proyek Analisis Data Publik E-Commerce dengan Python
+
+[Link dashboard](https://e-commerce-data-analyst.streamlit.app/)
+
+## Daftar Isi
+- [Gambaran](#gambaran)
+- [Struktur Proyek](#struktur-proyek)
+- [Penggunaan](#penggunaan)
+
+
+## Gambaran
+Proyek ini adalah sebuah proyek analisis data dan visualisasi yang berfokus pada data publik e-commerce. Ini mencakup kode untuk pengolahan data, analisis data eksploratif (EDA), dan dashboard Streamlit untuk eksplorasi data interaktif. Proyek ini bertujuan untuk menganalisis data pada Dataset Publik E-Commerce.
+
+## Struktur Proyek
+- `dashboard/`: Direktori ini berisi dashboard.py yang digunakan untuk membuat dashboard hasil analisis data.
+- `data/`: Direktori yang berisi file data CSV mentah.
+- `notebook.ipynb`: File ini digunakan untuk melakukan analisis data.
+- `README.md`: File dokumentasi ini.
+
+<!-- ## Instalasi
+1. Clone repositori ini ke Local Machine Anda: -->
+
+
+## Penggunaan
+1. **Data Wrangling**: Skrip pengolahan data tersedia dalam file `notebook.ipynb` untuk menyiapkan dan membersihkan data.
+
+2. **Exploratory Data Analysis (EDA)**: Jelajahi dan analisis data menggunakan skrip Python yang disediakan. Wawasan EDA dapat membimbing pemahaman Anda tentang pola data e-commerce public.
+
+3.  **Visualization**: Jalankan dashboard Streamlit untuk eksplorasi data interaktif:
+```
+cd submission/dashboard
+streamlit run dashboard.py
+```
+
+
---
+++ dashboard/dashboard.py
@@ -0,0 +1,209 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import streamlit as st
+import urllib
+from func import DataAnalyzer, BrazilMapPlotter
+
+sns.set(style='dark')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Dataset
+datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all_df = pd.read_csv("data/data_baru.csv")
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+# Geolocation Dataset
+geolocation = pd.read_csv('data/geolocation.csv')
+data = geolocation.drop_duplicates(subset='customer_unique_id')
+
+for col in datetime_cols:
+    all_df[col] = pd.to_datetime(all_df[col])
+
+min_date = all_df["order_approved_at"].min()
+max_date = all_df["order_approved_at"].max()
+
+# Sidebar
+with st.sidebar:
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write(' ')
+    with col2:
+        st.image("https://media.licdn.com/dms/image/D5603AQGccCpUKKpImA/profile-displayphoto-shrink_100_100/0/1708422726752?e=1715212800&v=beta&t=CnUwntsTz2a2moVyBbQFPbNfwSaNHfEj-U44OomP_j8"
+                 , width=100)
+    with col3:
+        st.write(' ')
+
+    # Date Range
+    start_date, end_date = st.date_input(
+        label="Pilih rentang waktu",
+        value=[min_date, max_date],
+        min_value=min_date,
+        max_value=max_date
+    )
+
+# Main
+main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
+                 (all_df["order_approved_at"] <= str(end_date))]
+
+function = DataAnalyzer(main_df)
+map_plot = BrazilMapPlotter(data, plt, mpimg, urllib, st)
+
+daily_orders_df = function.create_daily_orders_df()
+sum_spend_df = function.create_sum_spend_df()
+sum_order_items_df = function.create_sum_order_items_df()
+review_score, common_score = function.review_score_df()
+state, most_common_state = function.create_bystate_df()
+order_status, common_status = function.create_order_status()
+
+# Beri judul
+st.title("E-Commerce Public Data Analysis Dashboard")
+
+# Tambahkan deskripsi
+st.write("**Hasil analisis E-Commerce public data.**")
+
+# Order Harian
+st.subheader("Pesanan harian yang dikirim")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_order = daily_orders_df["order_count"].sum()
+    st.markdown(f"Total Order: **{total_order}**")
+
+with col2:
+    total_revenue = daily_orders_df["revenue"].sum()
+    st.markdown(f"Total pendapatan: **{total_revenue}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.lineplot(
+    x=daily_orders_df["order_approved_at"],
+    y=daily_orders_df["order_count"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# Pengeluran kustomer
+st.subheader("Pengeluaran kustomer")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_spend = sum_spend_df["total_spend"].sum()
+    st.markdown(f"Total pengeluaran: **{total_spend}**")
+
+with col2:
+    avg_spend = sum_spend_df["total_spend"].mean()
+    st.markdown(f"Rata rata pengeluaran: **{avg_spend}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.lineplot(
+    data=sum_spend_df,
+    x="order_approved_at",
+    y="total_spend",
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# Order Items
+st.subheader("Item pesanan")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["product_count"].sum()
+    st.markdown(f"Total item: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["product_count"].mean()
+    st.markdown(f"Rata rata item: **{avg_items}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette="viridis", ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("jumlah penjualan", fontsize=80)
+ax[0].set_title("Produk yang paling banyak terjual", loc="center", fontsize=90)
+ax[0].tick_params(axis ='y', labelsize=55)
+ax[0].tick_params(axis ='x', labelsize=50)
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="viridis", ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Jumlah penjualan", fontsize=80)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=90)
+ax[1].tick_params(axis='y', labelsize=55)
+ax[1].tick_params(axis='x', labelsize=50)
+
+st.pyplot(fig)
+
+# Skor Review
+st.subheader("Score Review")
+col1, col2 = st.columns(2)
+
+with col1:
+    avg_review_score = review_score.mean()
+    st.markdown(f"Rata rata skor: **{avg_review_score:.2f}**")
+
+with col2:
+    most_common_review_score = review_score.value_counts().idxmax()
+    st.markdown(f"Skor terbanyak: **{most_common_review_score}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+colors = sns.color_palette("viridis", len(review_score))
+
+sns.barplot(x=review_score.index,
+            y=review_score.values,
+            order=review_score.index,
+            palette=colors)
+
+plt.title("Review pelayanan kustomer", fontsize=15)
+plt.xlabel("Rating")
+plt.ylabel("Count")
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+# Menambahkan label di atas setiap bar
+for i, v in enumerate(review_score.values):
+    ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=12, color='black')
+
+st.pyplot(fig)
+
+# Demografik Kustomer
+st.subheader("Demografik kustomer")
+tab1, tab2 = st.tabs(["State", "Geolocation"])
+
+with tab1:
+    most_common_state = state.customer_state.value_counts().index[0]
+    st.markdown(f"Pesanan terbanyak: **{most_common_state}**")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=state.customer_state.value_counts().index,
+                y=state.customer_count.values, 
+                data=state,
+                palette="viridis"
+                    )
+
+    plt.title("Jumlah pelanggan berdasarkan kta", fontsize=15)
+    plt.xlabel("State")
+    plt.ylabel("Number of Customers")
+    plt.xticks(fontsize=12)
+    st.pyplot(fig)
+
+with tab2:
+    map_plot.plot()
+
+    with st.expander("Penjelasan"):
+        st.write('Berdasarkan grafik yang telah dibuat, terlihat bahwa ada lebih banyak pelanggan di wilayah tenggara dan selatan. Selain itu, ada juga lebih banyak pelanggan di kota-kota yang merupakan ibu kota, seperti São Paulo, Rio de Janeiro, Porto Alegre, dan beberapa kota lainnya.')
+
+st.caption('Copyright (C)2024 Muhammad Fachrurrozi')
